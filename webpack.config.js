c79cda7a2c43929f22003432a50f1a9a3ffc3f57
satyr <juliet917@126.com>
2017-08-11 15:51:16 +0800

    项目初始化脚手架搭建

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path=require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:'./src/js/main.js',
+    output:{
+        path:path.resolve(__dirname,'./dist'),
+        filename:'build.js'
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'index.html',
+            inject:'body'
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(scss|sass)$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:[
+                    {loader:'url-loader',options:{limit:5000}},
+                    'image-webpack-loader'
+                ]
+            },
+            {
+                test:/\.js/,
+                exclude:/node_module/,
+                use:[
+                    {
+                        loader:'babel-loader',
+                        options:{
+                            presets:['es2015'],
+                            plugins:['transform-runtime']
+                        }                        
+                    }
+                ]
+            },
+            {
+                test:/\.vue$/,
+                use:[
+                    'vue-loader'
+                ]
+            }
+        ]
+    }
+
+}
